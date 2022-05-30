6f957637ea63d388155bf2e428d9400b68390ca9
Austine D. Odhiambo <austine.declan@gmail.com>
2022-05-30 22:16:29 +0300

    Getting assertion errors in wsgiref/handlers.py when I try to run this

--- server.py
+++ server.py
@@ -7,8 +7,64 @@
 #
 #******************************************************************************
 
+#! /usr/bin/env python
+
+from wsgiref.simple_server import make_server
+from urllib.parse import parse_qs
+from html import escape
+
+html = """
+<html>
+<body>
+   <form method="post" action="">
+        <p>
+           Age: <input type="text" name="age" value="%(age)s">
+        </p>
+        <p>
+            Hobbies:
+            <input
+                name="hobbies" type="checkbox" value="software"
+                %(checked-software)s
+            > Software
+            <input
+                name="hobbies" type="checkbox" value="tunning"
+                %(checked-tunning)s
+            > Auto Tunning
+        </p>
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
+    <p>
+        Age: %(age)s<br>
+        Hobbies: %(hobbies)s
+    </p>
+</body>
+</html>
+"""
+
 def app(environ, start_response):
-    response_body = 'Request method: %s' % environ['REQUEST_METHOD']
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
+    
+    request_body = environ['wsgi.input'].read(request_body_size)
+    d = parse_qs(request_body)
+
+    age = d.get('age', [''])[0]         # Returns the first age value.
+    hobbies = d.get('hobbies', [])      # Returns a list of hobbies.
+
+    # Always escape user input to avoid script injection
+    age = escape(age)
+    hobbies = [escape(hobby) for hobby in hobbies]
+
+    response_body = html % { # Fill the above html template in
+        'checked-software': ('', 'checked')['software' in hobbies],
+        'checked-tunning': ('', 'checked')['tunning' in hobbies],
+        'age': age or 'Empty',
+        'hobbies': ', '.join(hobbies or ['No Hobbies?'])
+    }
 
     status = '200 OK'
 
@@ -18,5 +74,8 @@ def app(environ, start_response):
     ]
 
     start_response(status, response_headers)
-
     return[response_body]
+
+httpd = make_server('localhost', 8051, app)
+
+httpd.serve_forever()
